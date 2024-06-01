fbaad81c4a18822b5a81d40a73d16f21b8277ebc
RafaelRiveraRTC <Tropicalcone25@gmail.com>
2024-06-01 11:25:08 -0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment__ListView__and_all_other_Skills_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void addStudent_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void editSelectedStudent_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void deleteSelectedStudent_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
